4e9f2591f449733ef945427266d2e2d589cb6a88
saddansyah <saddanakbar@gmail.com>
2023-12-10 20:37:15 +0700

    feat: water

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int playerLives = 3;
+    void Awake()
     {
-        
+        // Find game manager in each scenes
+        int numGameManager = FindObjectsOfType<GameManager>().Length;
+
+        if (numGameManager > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ProcessPlayerDeath()
     {
-        
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetToFirstScene();
+        }
+    }
+
+    private void ResetToFirstScene()
+    {
+        // Back to first scene
+        SceneManager.LoadScene(0);
+        Destroy(this.gameObject);
+    }
+
+    private void TakeLife()
+    {
+        playerLives--;
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -8,26 +8,33 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D _rigidbody;
     private Animator _animator;
+    private CapsuleCollider2D _collider;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
     private Vector2 direction;
+    private bool isAlive = true;
 
     [SerializeField]
     private float _speed;
     [SerializeField]
     private float _rotationSpeed;
+    private float _reductionAmount = 120f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
+        if (!isAlive) return;
+
         FlipSprite();
         Walk();
+        DieWhen(_collider.IsTouchingLayers(LayerMask.GetMask("Enemy")));
     }
 
     private void Walk()
@@ -39,18 +46,6 @@ public class PlayerMovement : MonoBehaviour
                     0.1f);
 
         _rigidbody.velocity = _smoothedMovementInput * _speed;
-    }
-
-
-    private void RotateInDirectionOfInput()
-    {
-        if (_movementInput != Vector2.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
-            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-
-            _rigidbody.MoveRotation(rotation);
-        }
     }
 
     private void OnMove(InputValue inputValue)
@@ -65,39 +60,52 @@ public class PlayerMovement : MonoBehaviour
 
         // flip x 
         if (isPlayerMovementHorizontal)
-        {
             transform.localScale = new Vector2(-Mathf.Sign(_rigidbody.velocity.x), 1f);
-        }
 
         // is walking
         if (direction.x == 0 && direction.y == 0)
-        {
             _animator.SetBool("isWalking", false);
-        }
         else
-        {
             _animator.SetBool("isWalking", true);
-        }
+
 
         // flip sprite
         if (_animator.GetBool("isWalking"))
         {
             if (direction.y > 0)
-            {
                 _animator.SetInteger("direction", 1);
-            }
             if (direction.y < 0)
-            {
                 _animator.SetInteger("direction", -1);
-            }
             if (direction.y == 0)
-            {
                 _animator.SetInteger("direction", 0);
-            }
         }
+    }
 
-
+    void DieWhen(bool condition)
+    {
+        if (condition)
+        {
+            isAlive = false;
+            // Animator set to dying
+            _rigidbody.velocity = new Vector2(0, 0);
+        }
     }
 
 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Water")
+        {
+            _rigidbody.drag = _reductionAmount;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Water")
+        {
+            _rigidbody.drag = 0f;
+        }
+    }
 }
